91a665c0c9fad80d37798861821d58ee1f8ad281
creedtech <ayodeleayolaoke4@gmail.com>
2023-10-09 21:28:25 +0100

    changed background

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import detectMobileBrowser from 'detect-mobile-browser';
+// import detectMobileBrowser from 'detect-mobile-browser';
 
-const isMobile = detectMobileBrowser();
+// const isMobile = detectMobileBrowser();
 
 // if (!isMobile) {
 //   ReactDOM.createRoot(document.getElementById('root')).render(
